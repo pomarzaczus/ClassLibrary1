6146566cc3ef8e6ad07f5a561c638f0f9bfeca06
student <student@DESKTOP-5POI41D>
2025-03-06 10:27:55 +0100

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        float dod(float a, float b)
+        {
+
+            return a+b;
+        }
+        float odj(float a, float b)
+        {
+
+            return a - b;
+        }
+        float dzie(float a, float b)
+        {
+
+            return a / b;
+        }
+        float mno(float a, float b)
+        {
+
+            return a * b;
+        }
+    }
+}
