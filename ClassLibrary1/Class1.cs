a2f46225e8e6f99582ff8f0b6fc181b8ad1c0c17
szymon.m.olejniczak <szymon.m.olejniczak@student.put.poznan.pl>
2025-03-08 00:30:36 +0100

    zmiana z priv na pub

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -8,22 +8,22 @@ namespace kalulator
 {
     public class calculator
     {
-        float dod(float a, float b)
+        public float dod(float a, float b)
         {
 
             return a+b;
         }
-        float odj(float a, float b)
+        public float odj(float a, float b)
         {
 
             return a - b;
         }
-        float dzie(float a, float b)
+        public float dzie(float a, float b)
         {
 
             return a / b;
         }
-        float mno(float a, float b)
+        public float mno(float a, float b)
         {
 
             return a * b;
